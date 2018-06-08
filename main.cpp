7d5eb8d211c0f52c1d7901a21a9f68c1e45b936d
AnnaDoroshenko <annadoroshenko03@gmail.com>
2018-06-09 00:21:52 +0300

    task2 commit

--- main.cpp
+++ main.cpp
@@ -5,10 +5,18 @@
 void findMinMax(
         const std::vector<unsigned int>& array,
         unsigned int* min, unsigned int& max);
-std::pair<unsigned int, unsigned int> findMinMax(const std::vector<unsigned int>& array);
+// std::pair<unsigned int, unsigned int> findMinMax(const std::vector<unsigned int>& array);
 
 
 int main() {
+    unsigned int min;
+    unsigned int max;
+    std::vector<unsigned int> vect = {3, 4, 7, 9, 2};
+
+    findMinMax(vect, &min, max);
+
+    std::cout << "min = " << min << std::endl;
+    std::cout << "max = " << max << std::endl;
 
 
     return 0;
@@ -18,14 +26,20 @@ int main() {
 void findMinMax(
         const std::vector<unsigned int>& array,
         unsigned int* min, unsigned int& max) {
-    // min = arr[0];
-    // max = arr[0];
-    // for (unsigned int i = 1; i < arr.size(); i++) {
-    //     if(arr[i] )
-    // }
+    *min = array[0];
+    max = array[0];
+    for (unsigned int i = 1; i < array.size(); i++) {
+        unsigned int currentInt = *min;
+        if(array[i] < currentInt) {
+            *min = array[i];
+        }
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
 }
 
 
-std::pair<unsigned int, unsigned int> findMinMax(const std::vector<unsigned int>& array) {
-    return {};
-}
+// std::pair<unsigned int, unsigned int> findMinMax(const std::vector<unsigned int>& array) {
+//     return {};
+// }

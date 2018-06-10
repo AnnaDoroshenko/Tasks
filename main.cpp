3a214bb2fcf8967ef106318b69a83ee949fba627
Igor Boyarshin <igor.boyarshin@gmail.com>
2018-06-10 12:50:33 +0300

    fixmes

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ int main() {
     // const std::pair<unsigned int, unsigned int> minMaxpair;
 
     if (vect.size() != 0) {
+        // FIXME: you make your code evaluate the func findMinMax() twice (expensive!).
+        // Call once, store result, access .first and .second from the stored result.
         std::cout << "min = " << findMinMax(vect).first << std::endl;
         std::cout << "max = " << findMinMax(vect).second << std::endl;
     }
@@ -54,6 +56,16 @@ void findMinMax(
 std::pair<unsigned int, unsigned int> findMinMax(const std::vector<unsigned int>& array) {
     if (array.size() == 0) {
         std::cout << "Gimme numbers, clever one" << std::endl;
+        // FIXME: "boost" is a widely-known library for c++. Many things from there
+        // end up in the C++ standart.
+        // DO read more about std::optional and implement it here.
+        // However, std::optional is a part of C++ standart as of c++17
+        // (so no need to write "boost", just include the c++ library).
+        // In order to enable it, add "-std=c++17" into your Makefile into
+        // Compilation and Execution (after optimization flag).
+        // If your linter (vim) complains about it, find out how to add this
+        // compiler argument in your .vimrc for the linter.
+        // Also, why is there no ";" at the end??
         return // boost::optional<std::pair<unsigned int, unsigned int> > ();
     }
 
@@ -68,6 +80,9 @@ std::pair<unsigned int, unsigned int> findMinMax(const std::vector<unsigned int>
         }
     }
     
+    // FIXME: Good, but for simplicity use just {min, max}.
+    // The compiler understands the type of {min, max} because it knows the
+    // functiuon return type.
     return std::make_pair(min, max);
 }
 

0cd25facc6347fa714b544f5d53b8a9be144047d
AnnaDoroshenko <annadoroshenko03@gmail.com>
2018-06-12 00:08:53 +0300

    Improved second method

--- main.cpp
+++ main.cpp
@@ -14,13 +14,14 @@ int main() {
     unsigned int max;
     // const std::vector<unsigned int> vect = {3, 4, 7, 9, 2};
     const std::vector<unsigned int> vect = {};
-
+    const std::optional<std::pair<unsigned int, unsigned int>> foundPair = findMinMax(vect);
+    
     if (vect.size() != 0) {
-        const std::pair<unsigned int, unsigned int> minMaxPair = findMinMax(vect).value();
+        const std::pair<unsigned int, unsigned int> minMaxPair = foundPair.value();
         std::cout << "min = " << minMaxPair.first << std::endl;
         std::cout << "max = " << minMaxPair.second << std::endl;
     }
-    //
+
     // findMinMax(vect, &min, max);
     //
     // if (vect.size() != 0) {
@@ -37,6 +38,7 @@ void findMinMax(
         unsigned int* min, unsigned int& max) {
     if (array.size() == 0) {
         std::cout << "Gimme numbers, clever one" << std::endl;
+
         return;
     }
     *min = array[0];
@@ -55,17 +57,8 @@ void findMinMax(
 std::optional<std::pair<unsigned int, unsigned int>> findMinMax(const std::vector<unsigned int>& array) {
     if (array.size() == 0) {
         std::cout << "Gimme numbers, clever one" << std::endl;
-        // FIXME: "boost" is a widely-known library for c++. Many things from there
-        // end up in the C++ standart.
-        // DO read more about std::optional and implement it here.
-        // However, std::optional is a part of C++ standart as of c++17
-        // (so no need to write "boost", just include the c++ library).
-        // In order to enable it, add "-std=c++17" into your Makefile into
-        // Compilation and Execution (after optimization flag).
-        // If your linter (vim) complains about it, find out how to add this
-        // compiler argument in your .vimrc for the linter.
-        // Also, why is there no ";" at the end??
-        return std::optional<std::pair< unsigned int, unsigned int>> ();
+
+        return std::optional<std::pair<unsigned int, unsigned int>> ();
     }
 
     unsigned int min = array[0];

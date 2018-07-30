e59d586ad2e742bbf592dea329d8697541217867
AnnaDoroshenko <annadoroshenko03@gmail.com>
2018-07-30 23:48:47 +0300

    Possibility of traingle building is calculated

--- main.cpp
+++ main.cpp
@@ -1,68 +1,6 @@
 #include <iostream>
-/* #include <stdlib.h> // for srand(), rand() */
-/* #include <time.h> // for time() */
-
 
 int main() {
-    srand(time(0)); // for automatic random number generation
-    const float LENGTH = 1.0; // FIXME: 1.0 === 1.0d. You use float => write 1.0f
-    unsigned int count = 0;
-    unsigned int temp = 0;
-    const unsigned int PRESICION = 1000000;
-
-    while (count < PRESICION) {
-        float CHUNK_1;
-        float CHUNK_2;
-        float CHUNK_3;
-        const float POINT_1 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-        const float POINT_2 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-
-        if (POINT_1 > POINT_2) {
-            CHUNK_1 = POINT_2;
-            CHUNK_2 = POINT_1 - POINT_2;
-            CHUNK_3 = LENGTH - POINT_1;
-        } else {
-            CHUNK_1 = POINT_1;
-            CHUNK_2 = POINT_2 - POINT_1;
-            CHUNK_3 = LENGTH - POINT_2;
-        }
-
-        const float ARRAY_OF_CHUNKS[3] = {CHUNK_1, CHUNK_2, CHUNK_3};
-        float longestChunk = ARRAY_OF_CHUNKS[0];
-        unsigned int indexOfTheLongestChunk = 0;
-        for (unsigned int i = 1; i < 3; i++) {
-            const unsigned int currentChunk = ARRAY_OF_CHUNKS[i]; // FIXME: "unsigned int" -> "float" !!!
-            if (currentChunk > longestChunk) {
-                longestChunk = currentChunk; 
-                indexOfTheLongestChunk = i;
-            }
-        }
-
-        switch (indexOfTheLongestChunk) {
-            case 0:
-                if (CHUNK_1 < (CHUNK_2 + CHUNK_3)) {
-                    temp++;
-                }
-                break;
-            case 1:
-                if (CHUNK_2 < (CHUNK_1 + CHUNK_3)) {
-                    temp++;
-                }
-                break;
-            case 2:
-                if (CHUNK_3 < (CHUNK_1 + CHUNK_2)) {
-                    temp++;
-                }
-                break;
-            default:
-                break;
-        }
-
-        count++;
-        /* std::cout << ".... " << std::endl; */
-    };
-
-    std::cout << (float) temp / PRESICION << std::endl;
 
     return 0;
 } 
